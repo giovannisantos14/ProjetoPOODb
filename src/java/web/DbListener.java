1a73aa14ae1c7a07a5857a214acb292230f604b9
Giovanni <Giovanni@Giovanni-PC>
2020-06-21 16:27:47 -0300

    Estrutura básica do Projeto

---
+++ src/java/web/DbListener.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author Giovanni
+ */
+public class DbListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        
+    }
+}
